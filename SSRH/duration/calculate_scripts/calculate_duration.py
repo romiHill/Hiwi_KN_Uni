4e1992b1c4d69dba2435d7ec88732f5d2f10e0b2
Romi Hill <romi.hill8@gmail.com>
2022-12-19 10:20:16 +0100

    update python script to be same as jupyter notebook

--- SSRH/duration/calculate_scripts/calculate_duration.py
+++ SSRH/duration/calculate_scripts/calculate_duration.py
@@ -18,10 +18,12 @@ class Interval:
     """
     Class to store start point, end point, duration, and label of an interval
     """
+    # to do: may need to add info about previous and following segment, 
+    # ------ location of segment within word, syllable structure, etc
     def __init__(self, start_point = 0.0, end_point = 0.0, label = ""):
         self.start_point = float(start_point)
         self.end_point = float(end_point)
-        self.duration = self.calculate_duration()
+        self.duration = -1
         self.label = str(label)
     def calculate_duration(self):
         return (self.end_point - self.start_point)
@@ -37,10 +39,10 @@ def extract_info(interval, line):
     Extract relevant information from line and add it to Interval object
     """
     if 'xmin = ' in line:
-        start_point = re.sub(r'.*xmin = (\d*\.\d*)', '\\1', line)
+        start_point = re.sub(r'.*xmin = (\d*\.*\d*)', '\\1', line)
         interval.start_point = float(start_point)
     elif 'xmax = ' in line:
-        end_point = re.sub(r'.*xmax = (\d*\.\d*)', '\\1', line)
+        end_point = re.sub(r'.*xmax = (\d*\.*\d*)', '\\1', line)
         interval.end_point = float(end_point)
     elif 'text = ' in line:
         label = re.sub(r'.*text = "(.*)"', '\\1', line)
@@ -49,12 +51,14 @@ def extract_info(interval, line):
             interval.label = label
     return interval
 
+
 # ------------------------------------------------------------------------ #
 
 def read_input(in_folder_name = INPUT_PATH):
     """
     read in all textgrid files from folder as list of Interval objects
     """
+    intervals = []
     for file_name in os.listdir(in_folder_name):
         if not file_name.endswith('.TextGrid'):
             continue
@@ -62,36 +66,45 @@ def read_input(in_folder_name = INPUT_PATH):
         with open(os.path.join(in_folder_name, file_name), 'r', encoding='utf8') as in_file:
             is_phone_tier = False
             current_interval = Interval()
-            intervals = []
             for line in in_file:
+                # entered tier with phone segments
                 if PHONE_TIER in line:
                     is_phone_tier = True
+                # no longer in phone tier
                 elif 'name = ' in line:
                     is_phone_tier = False
-
-
+                
+                # extract info if in phone tier
                 if is_phone_tier:
                     current_interval = extract_info(current_interval, line)
+                # label has been filled, this interval is complete
                 if current_interval.label:
+                    # calculate duration and store it in the object
                     current_interval.duration = current_interval.calculate_duration()
+                    # add to list of interval objects and clear current interval
                     intervals.append(current_interval)
                     current_interval = Interval()
+    return intervals
 
-            return intervals
 
 # ------------------------------------------------------------------------ #
 # ------------ MAIN FUNCTION ------------ #
 # ------------------------------------------------------------------------ #
 def main():
+
+    # read in list of interval objects
     segment_intervals = read_input()
 
+    # convert list of interval objects to dataframe
     master_df = pd.DataFrame([interval.__dict__ for interval in segment_intervals])
 
-    #create a data frame dictionary to store your data frames
-    by_segment_df = {elem : pd.DataFrame() for elem in master_df.label.unique()}
+    # create dictionary of dataframes based on values in 'label' column
+    all_segments_df = {elem : pd.DataFrame() for elem in master_df.label.unique()}
 
-    for key in by_segment_df.keys():
-        by_segment_df[key] = master_df[:][master_df.label == key]
+    # fill values based on key
+    for key in all_segments_df.keys():
+        all_segments_df[key] = master_df[:][master_df.label == key]
+
 
 
     
